cf31fc7b535d6763dc04e5784a51f4af52bf1a2e
Prem Patil <55353041+iamprem18@users.noreply.github.com>
2020-09-30 01:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/",methods = ['GET','POST])
+def home():
+    if(request.method == 'GET'):
+        data = "Hello Prem"
+        return jsonify({'data':data})
+@app.route('/home/<int:num>',methods = ['GET'])
+def disp(num):
+    return jsonify({'Answer':num**2})
